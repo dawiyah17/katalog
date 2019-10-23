31645db3a9825603a270edeb0a158e0062422188
dawiyah17 <55434186+dawiyah17@users.noreply.github.com>
2019-10-23 18:00:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+ <!-- navbar -->
+ <div class="container"
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <style>
+        body {
+            background-color: white
+        }
+    </style>
+    <title>Katalog Lapak</title>
+    
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Clean In-klik</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav mr-auto">
+              <li class="nav-item active"></li>
+              <li class="nav-item">
+                 <a class="nav-link" href="#">Kategori</a>
+                 </li>
+              <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Antar jemput</a>
+                      <a class="dropdown-item" href="#">Tidak Antar Jemput</a>
+                  <div class="dropdown-divider">
+              </div>
+        </div>
+        </a>
+      </li>
+      <li><form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+    </form></li>
+    </ul>
+    <ul class="navbar-nav my-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pengaturan</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Akun</a>
+          <a class="dropdown-item" href="#">Lapak</a>
+          <div class="dropdown-divider"></div>
+        </div>
+        </a>
+      </li>
+      <button type="button" class="btn btn-info">Keluar</button>
+    </ul>
+  </div>
+</nav>
+<!--slide-->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="laundry.jpg" class="d-block w-100" alt="..." alt="" width="1000px" height="600px">>
+    </div>
+    <div class="carousel-item">
+      <img src="laundry.jpg" class="d-block w-100" alt="..." alt="" width="1000px" height="600px">>
+    </div>
+    <div class="carousel-item">
+      <img src="laundry.jpg" class="d-block w-100" alt="..." alt="" width="1000px" height="600px">>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+                </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+    </a>
+  </div>
+<!--card-->
+<div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="laundry.jpg" class="card-img-top" alt="..." height="250px">
+                        </div>
+                        <div class="col-md-6">
+                        
+                            <h5 class="card-title">laundry</h5>
+                            <p class="card-text">bla bla bla</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="laundry.jpg" class="card-img-top" alt="..." height="250px">
+                        </div>
+                        <div class="col-md-6">
+                        
+                            <h5 class="card-title">laundry</h5>
+                            <p class="card-text">bla bla bla</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+ </body>
+</html>
